b792a55a933363ff472ad57f974c0c12e9eee388
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-02-06 14:20:47 -0500

    initial commit

---
+++ Algos.java
@@ -0,0 +1,19 @@
+public class Algos{
+
+  void Print1to255(){
+    for(int i = 1; i < 256; i++){
+      System.out.printIn(i);
+    }
+  }
+    
+  void SumInts0to255(){
+    int sum = 0;
+    for(int i = 0; i < 256; i++){
+      sum += i;
+      System.out.printIn(sum);
+    }
+  }
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+  public static void main(String[] args)
+  {
+    Algos a =  new Algos();
+    System.out.printIn(a);
+    a.Print1to255();
+    // a.SumInts0to255();
+  }
+}
